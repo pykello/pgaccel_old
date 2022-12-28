fadef58d2a49ac8f5f5d4b9309f5102e52821bd7
Hadi Moshayedi <hadi@moshayedi.net>
2022-12-28 07:18:31 +0000

    Remove unused code

--- pgaccel.cc
+++ pgaccel.cc
@@ -10,34 +10,9 @@ using namespace std::chrono;
 #include "types.hpp"
 #include "columnar_table.h"
 
-#include <arrow/api.h>
-#include <arrow/filesystem/api.h>
-#include <arrow/util/bit_stream_utils.h>
-
-#include <parquet/types.h>
-#include <parquet/column_reader.h>
-#include <parquet/file_reader.h>
-
 const std::string path = "/home/hadi/data/tpch/1/parquet/lineitem.parquet";
 
 using namespace std;
-using arrow::Result;
-
-class EqFilterBase {
-public:
-    int column;
-    parquet::Type::type type;
-
-    std::string filterValueStr;
-};
-
-typedef std::unique_ptr<EqFilterBase> EqFilterUP;
-
-template<typename T>
-class EqFilter: public EqFilterBase {
-public:
-    typename T::c_type filterValue;
-};
 
 static uint64_t pow(uint64_t p, int q) {
     uint64_t result = 1;
@@ -73,120 +48,6 @@ ParseDecimal(int scale, const std::string &valueStr)
     }
 
     return whole * pow(10, scale) + decimal;
-}
-
-static Result<EqFilterUP>
-parseFilterDecimal(const parquet::DecimalLogicalType &decimalType,
-                   parquet::Type::type phyTy,
-                   int colIndex, const std::string &filterValue)
-{
-    int scale = decimalType.scale();
-    uint32_t whole, decimal;
-    size_t pos = filterValue.find(".");
-    if (pos == std::string::npos) {
-        whole = std::stoul(filterValue);
-        decimal = 0;
-    } else {
-        std::string wholeStr = filterValue.substr(0, pos);
-        std::string decimalStr = filterValue.substr(pos + 1);
-        // if decimal part has more digits than scale, trim
-        if (decimalStr.length() > scale) {
-            decimalStr = decimalStr.substr(0, scale);
-        }
-
-        // pad decimal part until its length is equal to scale
-        while (decimalStr.length() < scale) {
-            decimalStr += "0";
-        }
-
-        whole = std::stoul(wholeStr);
-        decimal = std::stoul(decimalStr);
-    }
-
-    switch (phyTy) {
-
-#define DECIMAL_TO_INT_CASE(T) \
-        case T::type_num: { \
-            auto eqFilter = std::make_unique<EqFilter<T>>(); \
-            eqFilter->column = colIndex; \
-            eqFilter->type = T::type_num; \
-            eqFilter->filterValue = whole * pow(10, scale) + decimal; \
-            return Result<EqFilterUP>(std::move(eqFilter)); \
-        }
-
-        DECIMAL_TO_INT_CASE(parquet::Int64Type);
-        DECIMAL_TO_INT_CASE(parquet::Int32Type);
-
-        // TODO:
-        // case parquet::FixedLenByteArray
-        // case parquet::ByteArrayType
-
-        default: {
-            std::cout << "Unsupported Physical Type for Decimals: "
-                      << parquet::TypeToString(phyTy) << std::endl;
-            exit(-1);
-        }
-    }
-}
-
-static Result<EqFilterUP>
-parseFilter(const std::string& filter, const parquet::SchemaDescriptor &schema)
-{
-    size_t pos = filter.find("=");
-    std::string colName = filter.substr(0, pos);
-    std::string filterValue = filter.substr(pos + 1);
-
-    int colIndex = -1;
-    for (int i = 0; i < schema.num_columns(); i++) {
-        if (schema.Column(i)->name() == colName) {
-            colIndex = i;
-            break;
-        }
-    }
-
-    if (colIndex == -1) {
-        return arrow::Status::Invalid("Invalid filter");
-    }
-
-    auto phyTy = schema.Column(colIndex)->physical_type();
-    auto logicalType = schema.Column(colIndex)->logical_type();
-
-    if (logicalType->is_decimal()) {
-        auto decimalType = static_cast<const parquet::DecimalLogicalType *>(logicalType.get());
-        return parseFilterDecimal(*decimalType, phyTy, colIndex, filterValue);
-    }
-
-    switch (phyTy) {
-        case parquet::Type::BYTE_ARRAY: {
-            auto eqFilter = std::make_unique<EqFilter<parquet::ByteArrayType>>();
-            eqFilter->column = colIndex;
-            eqFilter->type = parquet::Type::BYTE_ARRAY;
-            eqFilter->filterValueStr = filterValue;
-            eqFilter->filterValue =
-                parquet::ByteArray(filterValue.length(),
-                                   (const uint8_t *) eqFilter->filterValueStr.c_str());
-            return eqFilter;
-        }
-#define NUMERIC_FILTER_CASE(phyTy, fromStr) \
-        case phyTy::type_num: { \
-            auto eqFilter = std::make_unique<EqFilter<phyTy>>(); \
-            eqFilter->column = colIndex; \
-            eqFilter->type = phyTy::type_num; \
-            eqFilter->filterValueStr = filterValue; \
-            eqFilter->filterValue = fromStr(filterValue); \
-            return Result<EqFilterUP>(std::move(eqFilter)); \
-        }
-
-        NUMERIC_FILTER_CASE(parquet::Int32Type, std::stol);
-        NUMERIC_FILTER_CASE(parquet::Int64Type, std::stoll);
-        NUMERIC_FILTER_CASE(parquet::BooleanType, std::stol);
-
-        default: {
-            std::cout << "Unsupported Filter column type: " <<
-                parquet::TypeToString(phyTy) << std::endl;
-            exit(01);
-        }
-    }
 }
 
 int32_t
