f44ca14e8a3bbf54beb04e97fb28346c8f7c3ede
Hadi Moshayedi <hadi@moshayedi.net>
2023-01-05 01:57:26 +0000

    Move parquet loading to its own file

---
+++ src/parquet_import.cc
@@ -0,0 +1,193 @@
+#include <arrow/api.h>
+#include <arrow/filesystem/api.h>
+#include <arrow/util/bit_stream_utils.h>
+
+#include <parquet/types.h>
+#include <parquet/column_reader.h>
+#include <parquet/file_reader.h>
+
+
+#include <execution>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+#include "parquet_import.hpp"
+#include "columnar_table.h"
+#include "util.h"
+
+namespace pgaccel
+{
+
+std::vector<RowGroup>
+LoadParquetRowGroup(parquet::RowGroupReader& rowGroupReader,
+                    const ColumnarTable &columnarTable)
+{
+    std::vector<RowGroup> result;
+
+    int parquetColCount = rowGroupReader.metadata()->num_columns();
+    for (size_t colIdx = 0; colIdx < parquetColCount; colIdx++)
+    {
+        auto columnReader = rowGroupReader.Column(colIdx);
+        auto name = columnReader->descr()->name();
+        auto maybeColumnIdx = columnarTable.ColumnIndex(name);
+        if (!maybeColumnIdx.has_value())
+            continue;
+
+        std::vector<ColumnDataP> columnDataVec;
+
+        const auto &accelType = columnarTable.Schema()[*maybeColumnIdx].type;
+
+        switch (accelType->type_num()) {
+            case STRING_TYPE:
+                columnDataVec =
+                    GenerateDictColumnData<parquet::ByteArrayType, pgaccel::StringType>(
+                        *columnReader);
+                break;
+
+            case DATE_TYPE:
+                columnDataVec =
+                    GenerateDictColumnData<parquet::Int32Type, pgaccel::DateType>(
+                        *columnReader);
+                break;
+
+            case INT32_TYPE:
+                columnDataVec =
+                    GenerateRawColumnData<parquet::Int32Type, pgaccel::Int32Type>(
+                        *columnReader);
+                break;
+
+            case DECIMAL_TYPE:
+                columnDataVec =
+                    GenerateRawColumnData<parquet::Int64Type, pgaccel::DecimalType>(
+                        *columnReader);
+                break;
+
+            case INT64_TYPE:
+                columnDataVec =
+                    GenerateRawColumnData<parquet::Int64Type, pgaccel::Int64Type>(
+                        *columnReader);
+                break;
+        }
+
+        while (result.size() < columnDataVec.size())
+            result.push_back({});
+
+        for (int i = 0; i < columnDataVec.size(); i++)
+            result[i].columns.push_back(std::move(columnDataVec[i]));
+    }
+
+    return std::move(result);
+}
+
+std::unique_ptr<ColumnarTable> 
+ColumnarTable::ImportParquet(const std::string &tableName,
+                             const std::string &path,
+                             std::optional<std::set<std::string>> maybeFields)
+{
+    arrow::fs::LocalFileSystem fs;
+    auto openResult = fs.OpenInputFile(path);
+    if (!openResult.ok()) {
+        std::cout << "could not open file" << std::endl;
+        return {};
+    }
+
+    auto input = *openResult;
+    auto fileReader = parquet::ParquetFileReader::Open(input, parquet::default_reader_properties());
+    auto fileMetadata = fileReader->metadata();
+    auto parquetSchema = fileMetadata->schema();
+
+    std::set<std::string> fieldsToLoad;
+    if (maybeFields.has_value())
+    {
+        for(auto f: maybeFields.value())
+            fieldsToLoad.insert(ToLower(f));
+    }
+    else
+    {
+        for(size_t colIdx = 0; colIdx < parquetSchema->num_columns(); colIdx++)
+            fieldsToLoad.insert(ToLower(parquetSchema->Column(colIdx)->name()));
+    }
+
+    auto result = std::unique_ptr<ColumnarTable>(new ColumnarTable);
+    result->name_ = tableName;
+
+    for (size_t col = 0; col < parquetSchema->num_columns(); col++)
+    {
+        auto column = parquetSchema->Column(col);
+        if (0 == fieldsToLoad.count(ToLower(column->name())))
+            continue;
+
+        ColumnDesc columnDesc;
+        columnDesc.name = column->name();
+
+        auto phyType = column->physical_type();
+        auto logicalType = column->logical_type();
+
+        switch (phyType) {
+            case parquet::Type::BYTE_ARRAY:
+                columnDesc.type = std::make_unique<pgaccel::StringType>();
+                break;
+
+            case parquet::Type::INT32:
+                if (logicalType->is_date())
+                {
+                    columnDesc.type = std::make_unique<pgaccel::DateType>();
+                }
+                else
+                {
+                    columnDesc.type = std::make_unique<pgaccel::Int32Type>();
+                }
+                break;
+
+            case parquet::Type::INT64:
+                if (logicalType->is_decimal())
+                {
+                    auto parquetDecimalType =
+                        static_cast<const parquet::DecimalLogicalType *>(logicalType.get());
+                    auto accelDecimalType = std::make_unique<pgaccel::DecimalType>();
+                    accelDecimalType->scale = parquetDecimalType->scale();
+                    columnDesc.type = std::move(accelDecimalType);
+                }
+                else
+                {
+                    columnDesc.type = std::make_unique<pgaccel::Int64Type>();
+                }
+                break;
+
+            default:
+                std::cout << "Unsupported type: " << parquet::TypeToString(phyType) << std::endl;
+                return {};
+        }
+
+        result->schema_.push_back(std::move(columnDesc));
+    }
+
+    std::vector<int> rowGroupIdxs;
+    for (size_t i = 0; i < fileMetadata->num_row_groups(); i++)
+        rowGroupIdxs.push_back(i);
+
+    std::mutex push_mutex;
+    std::mutex log_mutex;
+    std::for_each(
+        std::execution::par_unseq,
+        rowGroupIdxs.begin(),
+        rowGroupIdxs.end(),
+        [&](int parquetGroup)
+        {
+            {
+                std::lock_guard lock(push_mutex);
+                std::cout << "Loading group " << parquetGroup << std::endl;
+            }
+            auto rowGroupReader = fileReader->RowGroup(parquetGroup);
+            auto rowGroups = LoadParquetRowGroup(*rowGroupReader, *result);
+
+            std::lock_guard lock(push_mutex);
+            for(auto &rowGroup: rowGroups)
+                result->row_groups_.push_back(std::move(rowGroup));
+        });
+
+    return result;
+}
+
+};
